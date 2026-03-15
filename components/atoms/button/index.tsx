58c7bebfbed497f23e5abc9f33f461fa954b8e57
Hazem Badran <hazemaqbadran@gmail.com>
2026-03-15 13:01:39 +0200

    fix button

--- components/atoms/button/index.tsx
+++ components/atoms/button/index.tsx
@@ -5,15 +5,15 @@ interface CustomButtonProps {
   message: string;
   isSubmitting: boolean;
   mode: 'text' | 'outlined' | 'contained';
-  disabled?: boolean;
-  isLoading?: boolean;
   onPress: () => Promise<void> | void;
 }
-const CustomButton = ({ message,disabled, isLoading, mode, onPress }: CustomButtonProps) => {
+
+const CustomButton = ({ message, isSubmitting, mode, onPress }: CustomButtonProps) => {
   return (
     <Button
-      loading={isLoading}
-      disabled={disabled}
+      testID="submit-button"
+      loading={isSubmitting}
+      disabled={isSubmitting}
       mode={mode}
       onPress={onPress}
       style={styles.button}
